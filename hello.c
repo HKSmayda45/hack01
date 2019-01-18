4e16c2e48fb689867f8a332246c0d1a0a26cafb7
HKSmayda45 <46770309+HKSmayda45@users.noreply.github.com>
2019-01-17 18:47:58 -0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/** 
+* Author : Holden Smayda and Asmita Jayswal
+* Date : 1/17/2019
+* Project : Hack 1
+*/ 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+	
+	//this hack prints the message hello 
+	printf("Hello");
+	
+	return 0;
+}
